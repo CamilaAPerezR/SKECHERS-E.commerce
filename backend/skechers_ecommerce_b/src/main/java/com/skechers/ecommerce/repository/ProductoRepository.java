8dde4f7b930cb1737b00d5566e2b7d8531676f58
mila0perez <camilandreaprodriguez06@gmail.com>
2026-08-22 13:52:31 -0500

    Agregar repository de Producto

---
+++ backend/skechers_ecommerce_b/src/main/java/com/skechers/ecommerce/repository/ProductoRepository.java
@@ -0,0 +1,10 @@
+package com.skechers.ecommerce.repository;
+
+import com.skechers.ecommerce.entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+}
+

ede36405aed8e8437a9f11661fab5e25b648aebf
mila0perez <camilandreaprodriguez06@gmail.com>
2026-08-26 09:33:37 -0500

    Actualizar seccion de productos destacados

--- frontend/skechers_ecommerce_f/src/features/productos/FeaturedProducts.jsx
+++ frontend/skechers_ecommerce_f/src/features/productos/FeaturedProducts.jsx
@@ -12,21 +12,35 @@ ProductCard para mantener la reutilización del código.
 ===========================================================
 */
 
+import { useEffect, useState } from "react";
 import "./FeaturedProducts.css";
+import { obtenerProductos } from "../../services/productoService";
+import ProductCard from "../../components/ui/ProductCard";
 
 function FeaturedProducts() {
 
-    return (
+    const [productos, setProductos] = useState([]);
 
+    useEffect(() => {
+        obtenerProductos()
+            .then((data) => {
+                setProductos(data);
+            })
+            .catch((error) => {
+                console.error("Error al obtener productos:", error);
+            });
+    }, []);
+
+        console.log("Productos recibidos:", productos);
+
+    return (
         <section className="featured-products">
 
             <div className="featured-header">
 
                 <span>NUEVA COLECCIÓN</span>
 
-                <h2>
-                    Productos destacados
-                </h2>
+                <h2>Productos destacados</h2>
 
                 <p>
                     Descubre algunos de los modelos más
@@ -39,14 +53,17 @@ function FeaturedProducts() {
 
             <div className="featured-grid">
 
-                {/* Aquí irán las ProductCard */}
+                {productos.map((producto) => (
+                    <ProductCard
+                        key={producto.id}
+                        producto={producto}
+                    />
+                ))}
 
             </div>
 
         </section>
-
     );
-
 }
 
 export default FeaturedProducts;
